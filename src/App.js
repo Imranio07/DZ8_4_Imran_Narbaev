90e3a44b0b49f12c0bb26a3886fe15c8a4117374
Imranio07 <kurutkg09@gmail.com>
2023-10-07 12:27:44 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+import Footer from './components/Footer';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Slider />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="src/components/Navbar#">Home</a></li>
+        <li><a href="src/components/Navbar#">Gallery</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
